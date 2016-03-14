a117bd4b6abe4ff9a5a1f14f2aa6f5a27a9ae890
Sann Chhan <sann.c.chhan@gmail.com>
2016-03-14 14:13:19 -0700

    Add titles and hover thumbnail hover effects

--- js/lightbox.js
+++ js/lightbox.js
@@ -5,24 +5,24 @@ var slackTheatre = {
 }
 
 var queryFlickr = function() {
+	// Clear contents after each search 
+	clearLightbox();
+	clearContainer();
+
 	// Get the query and form URL to access Flickr API	
-	
 	var apiUrl; 
 
 	var queryBox = document.getElementById('search-query');
 	
+
 	// Validate that a search query was received.
 	if(queryBox.value == undefined || queryBox.value == '') {
 		console.log('undefined query value. We will not be searching');
 		queryBox.style.backgroundColor = '#FFFF99';
-		queryBox.style.placeholder = "We'll need something here :)";
 		return;
 	} else {
 		apiUrl = getApiUrlString(queryBox.value);
 	}
-	
-	// Clear contents after each search 
-	clearLightbox();
 
 	xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -39,7 +39,7 @@ var queryFlickr = function() {
 var createThumbnails = function(data) {
 	// Variables for our images
 	var photo;
-	var thumbnail;
+	var thumbnailImg;
 
 	// Parse the response data and store into app namespace
 	slackTheatre.photos = JSON.parse(data).photos.photo;
@@ -50,20 +50,34 @@ var createThumbnails = function(data) {
 	for(var i=0; i<count; i++) {
 		photo = slackTheatre.photos[i];
 
-		thumbnail = createThumbnail(photo);
-		thumbnail.setAttribute('data-photoid', i);
+		// Create the img
+		thumbnailImg = createThumbnailImg(photo);
+		thumbnailImg.setAttribute('data-photoid', i);
+		thumbnailImg.addEventListener('mouseover', showThumbTitle);
+		thumbnailImg.addEventListener('mouseout', hideThumbTitle);
+
+		// Create the text title
+		var thumbTitle = document.createElement('p');
+		thumbTitle.setAttribute('class', 'thumbnail-title bottom');		
+
+		if(photo.title.length > 20) {
+			thumbTitle.innerHTML = photo.title.substring(0, 16) + "...";
+		} else {
+			thumbTitle.innerHTML = photo.title;
+		}
 
 		var thumbwrapper = document.createElement('div');
 		thumbwrapper.setAttribute('class', 'thumbnail-wrapper');
 
-		thumbwrapper.appendChild(thumbnail);
+		thumbwrapper.appendChild(thumbnailImg);
+		thumbwrapper.appendChild(thumbTitle);
 
 	    document.getElementById('container').appendChild(thumbwrapper);	
 	}
 }
 
-var createThumbnail = function(photoObj) {
-	var thumbnail = document.createElement("img");
+var createThumbnailImg = function(photoObj) {
+	var thumbnail = document.createElement('img');
 
 	thumbnail.setAttribute('src', getImgUrlString(photoObj, 'n'));
 	thumbnail.setAttribute('alt', photoObj.title);
@@ -99,7 +113,12 @@ var closeLightbox = function() {
 	document.getElementById('fade').style.display = 'none';
 }
 
-
+var clearContainer = function () {
+	var container = document.getElementsByClassName('container')[0];
+	while(container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
 
 var clearLightbox = function() {
 	var lbContent = document.getElementById('lightbox-content');
@@ -137,15 +156,22 @@ var loadLbContent = function(photoId) {
 
 	var photoObj = slackTheatre.photos[photoId];
 	console.log(photoObj);
-	
-	var content = document.createElement("img");
-	content.setAttribute('src', getImgUrlString(photoObj, 'c'));
+
+	// Create the lightbox content
+	var lbTitle = document.createElement('p');
+	lbTitle.setAttribute('class', 'lb-title center-horizontal');
+	lbTitle.innerHTML = photoObj.title;
+
+	var content = document.createElement('img');
+	content.setAttribute('src', getImgUrlString(photoObj, 'z'));
 	content.setAttribute('class', 'lightbox-img');
 	content.setAttribute('data-photoid', photoId);
 
 	var lbContent = document.getElementById('lightbox-content');
 	lbContent.appendChild(content);
+	lbContent.appendChild(lbTitle);
 
+	// Navigation of lightbox
 	var prevButton = document.getElementById('prev');
 	var nextButton = document.getElementById('next');
 
@@ -162,4 +188,29 @@ var loadLbContent = function(photoId) {
 	} else {
 		nextButton.style.visibility = 'visible';
 	}
+}
+
+// var showThumbTitle = function() {
+// 	console.log('Showing the thumbnail title');
+// 	console.log(this);
+
+// 	var myTitle = document.getElementsByClassName('thumbnail-title')[this.getAttribute('data-photoid')];
+// 	myTitle.style.visibility = 'visible';
+
+// }
+
+var showThumbTitle = function() {
+	console.log('Showing the thumbnail title');
+	console.log(this);
+
+	var myTitle = document.getElementsByClassName('thumbnail-title')[this.getAttribute('data-photoid')];
+
+	console.log(myTitle.classList);
+	myTitle.style.visibility = 'visible';
+
+}
+
+var hideThumbTitle = function() {
+	var myTitle = document.getElementsByClassName('thumbnail-title')[this.getAttribute('data-photoid')];
+	myTitle.style.visibility = 'hidden';
 }
